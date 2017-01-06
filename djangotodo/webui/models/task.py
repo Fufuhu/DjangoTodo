4ed777d48281c4448ffa6a8618ccaa0caf2fc0e6
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-07 01:12:49 +0900

    モデルのモジュールを分割できるよう修正

---
+++ djangotodo/webui/models/task.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Task(models.Model):
+    """
+    Task to do.
+    """
+    name = models.CharField(max_length=30)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    memo = models.CharField(max_length=200)
+
+    class Meta:
+        app_label = 'webui'
