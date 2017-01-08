2f514bf7612a00bc490ea48c49fa829a216dec62
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-08 23:51:08 +0900

    Task作成用フォームを準備

---
+++ djangotodo/webui/forms/task_form.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from webui.models.task import Task
+
+class TaskCreateForm(ModelForm):
+    """
+    Form class for Task model
+    """
+    class Meta:
+        model = Task
+        fields = ('name', 'startTime', 'endTime', 'memo', )
