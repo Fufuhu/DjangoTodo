cd0995453134c63272e02a06f54f022d7f89a225
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-08 21:45:43 +0900

    ステータス列が管理画面で表示されるよう修正

--- djangotodo/webui/admin.py
+++ djangotodo/webui/admin.py
@@ -4,5 +4,5 @@ from webui.models import Task
 # Register your models here.
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'startTime', 'endTime', 'memo',)
+    list_display = ('id', 'name', 'startTime', 'endTime','status', 'memo',)
 admin.site.register(Task, TaskAdmin)
