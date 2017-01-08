e0753bf11cd284194c485ce5bced34ca7c24c2ac
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-09 00:49:41 +0900

    blankの可否を記述

--- djangotodo/webui/models/task.py
+++ djangotodo/webui/models/task.py
@@ -9,9 +9,9 @@ class Task(models.Model):
     """
     Task to do.
     """
-    name = models.CharField('タスク名', max_length=30)
-    startTime = models.DateTimeField('開始時刻')
-    endTime = models.DateTimeField('終了時刻')
-    memo = models.CharField('メモ', max_length=200)
-    status = models.CharField('状態', max_length=20, default='未着手')
+    name = models.CharField('タスク名', max_length=30, blank=False)
+    startTime = models.DateTimeField('開始時刻', blank=True)
+    endTime = models.DateTimeField('終了時刻', blank=True)
+    memo = models.CharField('メモ', max_length=200, blank=True)
+    status = models.CharField('状態', max_length=20, default='未着手', blank=False)
 
