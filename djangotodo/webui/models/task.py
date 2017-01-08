01096e69070df839aaee456a1d71a26bc6146b98
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-09 00:29:31 +0900

    タスクのカラムを日本語名で記述

--- djangotodo/webui/models/task.py
+++ djangotodo/webui/models/task.py
@@ -1,13 +1,17 @@
+"""
+Model for Task
+"""
 from django.db import models
+
 
 
 class Task(models.Model):
     """
     Task to do.
     """
-    name = models.CharField(max_length=30)
-    startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
-    memo = models.CharField(max_length=200)
-    status = models.CharField(max_length=20, default='Not started')
+    name = models.CharField('タスク名', max_length=30)
+    startTime = models.DateTimeField('開始時刻')
+    endTime = models.DateTimeField('終了時刻')
+    memo = models.CharField('メモ', max_length=200)
+    status = models.CharField('状態', max_length=20, default='未着手')
 
