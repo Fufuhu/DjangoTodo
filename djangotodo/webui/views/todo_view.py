2f9ce4d35b121336ed993e4bd88f2ed840d75f7b
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2017-01-08 21:12:21 +0900

    Taskをビューの情報に含めた

--- djangotodo/webui/views/todo_view.py
+++ djangotodo/webui/views/todo_view.py
@@ -2,6 +2,8 @@ from django.views.generic import View
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from webui.models.task import Task
+
 class TodoListView(View):
     """
     List up the list of Todo
@@ -9,7 +11,8 @@ class TodoListView(View):
     def get(self, request):
         """Return the list of to do(plan)"""
         # return HttpResponse('Todoの一覧')
-        return render(request, 'webui/todo_list.html')
+        tasks = Task.objects.all()
+        return render(request, 'webui/todo_list.html', {'tasks': tasks})
 
 
 
